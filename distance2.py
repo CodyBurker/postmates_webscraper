86e89c831ee3e269bf22eab367727c50da750acc
margoJS <76970352+margoJS@users.noreply.github.com>
2021-11-18 21:15:51 -0500

    Added forward geocoding api and geodesic code

--- distance2.py
+++ distance2.py
@@ -20,50 +20,74 @@ def get_distance(file):
 
     df = file
     addresses = []
+    latitudes = []
+    longitudes = []
+    distance = []
 
     print(df.Link)
+    
+    #Forward geocoding restaurant address
+    api_key = "dd044ee1e656909e96c479030d3c087a"
 
-
-    for link in df.Link:
+    for ind in df.index:
+        link = df['Link'][ind]
         driver.get(link)
 
         action = ActionChains(driver)
         action.send_keys(Keys.ESCAPE);
         action.perform()
 
-        distances = driver.find_elements_by_xpath("//p[@class='ba bv dm bw bx']")
+        distances = driver.find_elements(By.XPATH,".//p[@class='ba bv do bw bx']")
         distances = [distance.text for distance in distances]
+        #print(distances)
         distances = re.split("\n",distances[0])[0]
+        #print(distances)
         addresses.append(distances)
+        
+        # Forward Geocoding API Endpoint
+        query = {"access_key" : api_key, "query" : distances, "limit" : 1}
+        response = requests.get('http://api.positionstack.com/v1/forward', params = query)
+        jsonRest = (response.json())
+        #Documentation: https://positionstack.com/documentation
+        
+        #Get latitude and longitude from json data
+        latLong = jsonRest['data']
+        #print(latLong)
+        latLong = latLong[0]
+        lat = latLong['latitude']
+        #print(lat)
+        latitudes.append(lat)
+        long = latLong['longitude']
+        #print(long)
+        longitudes.append(long)
+        
+        # Get the distance calculated in miles
+        restaurant = (lat, long)
+        #print("Restaurant address: ", restaurant)
+        delivAddress = (df['Latitude'][ind], df['Longitude'][ind])
+        #print("Delivery address: ", delivAddress)
+        dist = geodesic(restaurant, delivAddress).mi
+        #print("Distance: ", dist)
+        distance.append(dist)
     
     # Close driver
     driver.close()
     df['Restaurant Address'] = addresses
-    
-    #Forward geocoding restaurant address
-    api_key = "dd044ee1e656909e96c479030d3c087a"
-    # Forward Geocoding API Endpoint
-    query = {"access_key" : api_key, "query" : "1600 Pennsylvania Ave NW, Washington DC"}
-    response = requests.get('http://api.positionstack.com/v1/forward', params = query)
-    print(response.json())
-    #Documentation: https://positionstack.com/documentation
-    
-    # Loading the lat-long data for restaurant & delivAddress
-    restaurant = (22.5726, 88.3639)
-    delivAddress = (28.7041, 77.1025)
-    
-    # Print the distance calculated in miles
-    print(geodesic(restaurant, delivAddress).mi)
+    df['Restaurant Latitude'] = latitudes
+    df['Restaurant Longitude'] = longitudes 
+    df['Distance'] = distance
     
     # Return dataframe
     return df
 
 
 if __name__ == '__main__':
+    start = time.time()
     with open('postmates_scrape.csv') as csvfile:
         file = pd.read_csv(csvfile)
         df = get_distance(file)
     print(df)
-    # price = get_price(link)
-    # print(f"Resteraunt: {df.iloc[0]['Name']}")
-    # print(f"Price: {price}")
+    df.to_csv('distance_scrape.csv', index=False)
+    end = time.time()
+    time_elapsed = end-start
+    print("Runtime: ", time_elapsed)
